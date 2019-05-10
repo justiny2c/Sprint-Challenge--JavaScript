85a6c34d483c021fa0a16c66ad308ab48406db13
justiny2c <justiny2c@gmail.com>
2019-05-10 12:32:47 -0700

    Forgot to do .forEach, I used indexing

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -131,9 +131,14 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
-for (let i = 0; i < zooAnimals.length; i++) {
-  animalNames.push(`Name: ${zooAnimals[i].animal_name}, Scientific: ${zooAnimals[i].scientific_name}`)
-}
+
+// for (let i = 0; i < zooAnimals.length; i++) {
+//   animalNames.push(`Name: ${zooAnimals[i].animal_name}, Scientific: ${zooAnimals[i].scientific_name}`)
+// }
+
+zooAnimals.forEach(animal => {
+  animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`)
+});
 
 console.log(animalNames);
 
